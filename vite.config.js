7c61a45ea8506a5625ab4fd6537571c8499a136c
River <river.schenck@frontify.com>
2025-07-17 12:58:39 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: './public/finder.html', // 👈 your Trello modal entry point
-    },
-  },
 });
