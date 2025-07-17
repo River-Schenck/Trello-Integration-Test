1c2dc218195e22e18101e405495a1b33c1db4033
River <river.schenck@frontify.com>
2025-07-17 12:48:30 -0400

    Initial commit of Trello Power-Up with Frontify Finder

---
+++ public/client.js
@@ -0,0 +1,19 @@
+TrelloPowerUp.initialize({
+    'card-buttons': function (t) {
+      return [
+        {
+          text: 'Attach from Frontify',
+          icon: 'https://media.ffycdn.net/eu/demo/43L2Li7yr59XJTunCL2w.svg',
+          callback: function (t) {
+            return t.modal({
+              url: 'finder.html',
+              accentColor: '#cbbbfb',
+              title: 'Select an Asset from Frontify',
+              fullscreen: true,
+            });
+          },
+        },
+      ];
+    },
+  });
+  
