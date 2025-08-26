46d4a8d0a0815a85f0bf62c7c6063b9e7676313c
River <river.schenck@frontify.com>
2025-08-26 17:31:23 +0200

    save attachment test

--- public/client.js
+++ public/client.js
@@ -15,5 +15,21 @@ TrelloPowerUp.initialize({
         },
       ];
     },
+    'save-attachment': function (t, options) {
+    return {
+      name: 'Save to Frontify (test)',
+      callback: async function (t, opts) {
+        const att = (opts && opts.attachment) || (options && options.attachment);
+        // Quick sanity logs
+        console.log('[SaveAttachment] attachment:', att);
+        console.log('[SaveAttachment] url:', att?.url);
+        console.log('[SaveAttachment] name:', att?.name);
+        console.log('[SaveAttachment] bytes:', att?.bytes);
+        console.log('[SaveAttachment] mimeType:', att?.mimeType);
+
+        t.alert({ message: 'Check the console for attachment info 👀', duration: 5 });
+      },
+    };
+  },
   });
   
